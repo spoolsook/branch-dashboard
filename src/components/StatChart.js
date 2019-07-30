b0f811a6d2c58a5ce6f40a69f449c603b7ba7860
spoolsook <pssarayutz@gmail.com>
2019-07-30 11:52:35 +0700

    3. ใส่ Line Chart จากตัวอย่าง

--- src/components/StatChart.js
+++ src/components/StatChart.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import {Card} from 'antd';
+import {Card} from 'antd';import Chart from 'react-google-charts';
 
 export default class StatChart extends Component {
     render() {
@@ -8,7 +8,28 @@ export default class StatChart extends Component {
                 <Card title="Chart" style={{
                     width: '100%'
                 }}>
-                    <p>Card content</p>
+                  <Chart
+                        width={'100%'}
+                        height={'400px'}
+                        chartType="Line"
+                        loader={<div>Loading Chart</div>}
+                        data={[
+                            ['x', 'dogs'],
+                            [0, 0],
+                            [1, 10],
+                            [2, 23],
+                            [3, 17],
+                            [4, 18],
+                            [5, 9],
+                            [6, 11],
+                            [7, 27],
+                            [8, 33],
+                            [9, 40],
+                            [10, 32],
+                            [11, 35],
+                        ]}
+                        rootProps={{ 'data-testid': '1' }}
+                    />
                 </Card>
             </div>
         )
