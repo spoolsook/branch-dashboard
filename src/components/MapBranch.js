a9158e4a9ca26743b55f6b633b5e4447be3ed2b9
spoolsook <pssarayutz@gmail.com>
2019-07-30 11:24:13 +0700

    6. ปักหมุดเมื่อแผนที่โหลดเสร็จ

--- src/components/MapBranch.js
+++ src/components/MapBranch.js
@@ -1,29 +1,42 @@
 import React, { Component } from 'react'
-import {} from 
+import GoogoleMapReact from 'google-map-react';
 
 export default class MapBranch extends Component {
 
     static defaultProps = {
         // Kerry Siam Seaport Location
         center: {
-          lat: 13.7200452,
-          lng: 100.5135078
+            lat: 13.7200452,
+            lng: 100.5135078
         },
         zoom: 15
-      };
+    };
+
+    handleApiLoaded(map, maps) {
+        let marker = new maps.Marker({
+            position: this.props.center,
+            map,
+            title: 'Kerry'
+        });
+      }
 
     render() {
-        return (
-            <div>
+        return (            
+            <div style={{
+                height: '100vh',
+                width: '100%'
+              }}>
                 <GoogoleMapReact
                     bootstrapURLKeys={
                         {
-                            key:'AIzaSyBDqlW1EIlePcA48oLVV_kYQJXm9dQ75uw'
+                            key: 'AIzaSyBDqlW1EIlePcA48oLVV_kYQJXm9dQ75uw'
                         }
                     }
-                    defaultCenter={this.defaultProps.center}
-                    defaultZoom={this.defaultProps.zoom}
-                >                    
+                    defaultCenter={this.props.center}
+                    defaultZoom={this.props.zoom}
+                    yesIWantToUseGoogleMapApiInternals
+                    onGoogleApiLoaded={({ map, maps }) => this.handleApiLoaded(map, maps)}
+                >
                 </GoogoleMapReact>
             </div>
         )
