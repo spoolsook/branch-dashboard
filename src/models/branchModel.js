6b436240dc8b171ba2d970a5610ce931f2b3b4da
spoolsook <pssarayutz@gmail.com>
2019-07-30 15:02:28 +0700

    1. เพิ่มรายละเอียดของข้อมูลสาขา

--- src/models/branchModel.js
+++ src/models/branchModel.js
@@ -1,17 +1,41 @@
 const headquarter = {
+    id: 1,
     name: 'Silom',
     position: {
       lat: 13.7200452,
       lng: 100.5135078
+    },
+    chartData: {
+      chartField: ['Month','Amount'],
+      datas: [
+        { month: 'February', amount: 10392 },
+        { month: 'March', amount: 18239 },
+        { month: 'April', amount: 14290 },
+        { month: 'May', amount: 23912 },
+        { month: 'June', amount: 26167 },
+        { month: 'July', amount: 28199 },
+      ]
     }
   }
   
   let branches = [
     headquarter, {
+      id: 2,    
       name: 'Cholburi',
       position: {
         lat: 13.1247584,
         lng: 100.9133127
+      },
+      chartData: {
+        chartField: ['Month','Amount'],
+        datas: [
+          { month: 'February', amount: 70032 },
+          { month: 'March', amount: 54789 },
+          { month: 'April', amount: 62789 },
+          { month: 'May', amount: 89272 },
+          { month: 'June', amount: 100328 },
+          { month: 'July', amount: 128903 },
+        ]
       }
     }
   ]
