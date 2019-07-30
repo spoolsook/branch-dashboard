ca25f346f473f51c88bfcb1de4a6943420ba7df6
spoolsook <pssarayutz@gmail.com>
2019-07-30 11:18:44 +0700

    5. จัด Layout แผนที่ กับ Chart ใน App.js

--- src/App.js
+++ src/App.js
@@ -3,26 +3,28 @@ import logo from './logo.svg';
 import './App.css';
 import HeaderBar from './components/HeaderBar';
 
-import {Layout, Menu, Breadcrumb} from 'antd';
+import { Layout, Menu, Breadcrumb, Row, Col } from 'antd';
 import MapBranch from './components/MapBranch';
-const {Header, Content, Footer} = Layout;
+const { Header, Content, Footer } = Layout;
 
 function App() {
   return (
     <div>
-         <Layout className="layout">
-        <HeaderBar/>
+      <Layout className="layout">
+        <HeaderBar />
         <Content style={{
           padding: '0 50px'
         }}>
           <div
             style={{
-            background: '#fff',
-            padding: 24,
-            minHeight: 280
-          }}>
-            <MapBranch />
-
+              background: '#fff',
+              padding: 24,
+              minHeight: 280
+            }}>
+            <Row gutter={16}>
+              <Col span={12}><MapBranch /></Col>
+              <Col span={12}>Chart</Col>
+            </Row>
           </div>
         </Content>
         <Footer style={{
--- src/components/MapBranch.js
+++ src/components/MapBranch.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react'
+import {} from 
 
 export default class MapBranch extends Component {
+
+    static defaultProps = {
+        // Kerry Siam Seaport Location
+        center: {
+          lat: 13.7200452,
+          lng: 100.5135078
+        },
+        zoom: 15
+      };
+
     render() {
         return (
             <div>
-                map
+                <GoogoleMapReact
+                    bootstrapURLKeys={
+                        {
+                            key:'AIzaSyBDqlW1EIlePcA48oLVV_kYQJXm9dQ75uw'
+                        }
+                    }
+                    defaultCenter={this.defaultProps.center}
+                    defaultZoom={this.defaultProps.zoom}
+                >                    
+                </GoogoleMapReact>
             </div>
         )
     }
